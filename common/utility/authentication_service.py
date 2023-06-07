7a185818c476c57bac34cbcc39906cacb2701a17
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 23:58:16 +0530

    Update authentication_service.py

--- common/utility/authentication_service.py
+++ common/utility/authentication_service.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from userprofile.models import UserProfile
 
+
 def get_tokens_for_user(user: UserProfile) -> dict:
     refresh = RefreshToken.for_user(user)
 
     return get_access_from_refresh_token(str(refresh))
+
 
 def get_access_from_refresh_token(refresh: str) -> dict:
     try:
@@ -17,6 +19,7 @@ def get_access_from_refresh_token(refresh: str) -> dict:
         }
     except:
         return {'error': 'Token Expired'}
+
 
 def get_user_for_request(request) -> UserProfile:
     auth = JWTAuthentication()
